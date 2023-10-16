f87fbf856da5e5aeb9f95edc3e81affe60cb69df
ch010402 <ZBook@DESKTOP-JDAJDT3>
2023-10-16 22:00:40 +0200

    added comments

--- blink/blink_object.cpp
+++ blink/blink_object.cpp
@@ -5,28 +5,37 @@
 #include <gpiod.h> // library to access the GPIO Pins on a raspberryPI
 #include <unistd.h> // used for usleep
 #include <string> // string class
-#include <regex> // used for checking regualar expressions
 #include <iostream> // cout
-#include <stdexcept>
 
-// variable
+//// variable
+
+// counter for how many lines to the io chip are opend 
 int openLineCounter = 0;
+// enables disbles debug console output 
 bool debugOutput = true;
-// enumeration
+
+//// enumeration
+
+// ioType enumerate possible values {pump, valve, breaker, led}
 enum ioType {
   pump, valve, breaker, led
 };
 
-// classes 
+//// classes
 
 // io class (string ioName, int gpioPin, enum ioType { pump, valve, breaker })
 class io {
 private:
+  // GPIO pin to access 
   int ioPin;
+  // boolean to check wheter the io is setup or needs to bi initilaized 
   bool initialized = false;
 public:
+  // io name defined as std::string used for output
   std::string name;
+  // io status default to false meaning off not set, reports the status as boolean 
   bool status = false;
+  // ioType enumerate possible values {pump, valve, breaker, led}
   ioType typ;
   // constructor
   io(std::string ioName, int gpioPin, ioType type) {
@@ -34,17 +43,19 @@ public:
 	ioPin = gpioPin;
 	typ = type;
   }
-  // member functinons
+  // io.on() function switches the io on 
   void on() {
 	if (!initialized) initialize();
 	gpiod_line_set_value(line, 1);
 	status = true;
   }
+  // io.off() function switches the io off 
   void off() {
 	if (!initialized) initialize();
 	gpiod_line_set_value(line, 0);
 	status = false;
   }
+  // io.toggle() function switches the state of the io from off to on and from on to off
   void toggle() {
 	if (!initialized) initialize();
 	if (status == false) {
@@ -56,6 +67,7 @@ public:
 	  status = false;
 	}
   }
+  // io.destroy() function closes the line to the io and if it is the last line the connection to the chip 
   void destroy() {
 	gpiod_line_release(line); // close GPIO line
 	openLineCounter--;
@@ -69,13 +81,20 @@ public:
 	initialized = false;
   }
 private:
-  struct gpiod_chip* chip; // build gpiod lines
-  struct gpiod_line* line; // build gpiod lines
+  // build gpiod lines
+  struct gpiod_chip* chip; 
+  // build gpiod lines
+  struct gpiod_line* line;
+  // io.initialize() builds up the connection to the chip and creates a line to the io
   void initialize() {
-	const char* chipname = "gpiochip0"; // set the chip name as const
-	chip = gpiod_chip_open_by_name(chipname); // open GPIO chip
-	line = gpiod_chip_get_line(chip, ioPin); // open GPIO line
-	gpiod_line_request_output(line, "output", 0); // request line as output
+	// set the chip name as const
+	const char* chipname = "gpiochip0"; 
+	// open GPIO chip
+	chip = gpiod_chip_open_by_name(chipname); 
+	// open GPIO line
+	line = gpiod_chip_get_line(chip, ioPin); 
+	// request line as output
+	gpiod_line_request_output(line, "output", 0); 
 	openLineCounter++;
 	if (debugOutput)
 	  std::cout <<"Line " << openLineCounter << " opend." << std::endl;
@@ -83,12 +102,16 @@ private:
   }
 };
 
-// MAIN
+//// MAIN
 int main(int argc, char** argv) {
-  // setup
-  io rot1("rot1", 23, pump), blue("blue", 22, led);
+  //// setup
+
+  // build io objects 
+  io rot1("rot1", 23, pump), 
+	blue("blue", 22, led);
+  
+  // loop; i = loop counter
   int i = 0;
-  // loop
   while (i < 6) {
 	rot1.toggle();
 	blue.on();
