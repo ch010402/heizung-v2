1ce8ee06c6d86bc4760b06cdbb757c4129c8102a
ch010402 <ZBook@DESKTOP-JDAJDT3>
2023-10-20 20:09:47 +0200

    fix

--- time/time.cpp
+++ time/time.cpp
@@ -3,7 +3,7 @@
 
 auto now = std::chrono::system_clock::now();
 std::time_t in_time_t = std::chrono::system_clock::to_time_t(now);
-std:tm buffer;
+std::tm buffer;
 localtime_r(&in_time_t, &buffer);
 
-std::cout << now;
+//std::cout << now;
