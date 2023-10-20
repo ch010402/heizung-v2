3a876c8b078fd1329802c07967f3a976378a5736
ch010402 <ZBook@DESKTOP-JDAJDT3>
2023-10-20 20:55:33 +0200

    umbau

--- time/ntimecheck.cpp
+++ time/ntimecheck.cpp
@@ -15,13 +15,15 @@ int checkNiederTarif() {
   std::tm* currentTime_tm = std::localtime(&currentTime_t);
 
   // Extract individual components
-  int year = currentTime_tm->tm_year + 1900;  // Years since 1900
-  int month = currentTime_tm->tm_mon + 1;     // Month (0-based index)
-  int day = currentTime_tm->tm_mday;          // Day of the month
+//  int year = currentTime_tm->tm_year + 1900;  // Years since 1900
+//  int month = currentTime_tm->tm_mon + 1;     // Month (0-based index)
+//  int day = currentTime_tm->tm_mday;          // Day of the month
   int hour = currentTime_tm->tm_hour;         // Hour
-  int minute = currentTime_tm->tm_min;        // Minute
-  int second = currentTime_tm->tm_sec;        // Second
+//  int minute = currentTime_tm->tm_min;        // Minute
+//  int second = currentTime_tm->tm_sec;        // Second
   int weekday = currentTime_tm->tm_wday;      // days since sunday
+
+  std::cout << currenTimeStr << std::endl;
 
   if (weekday == 0 || weekday == 6) {
 	return true;
@@ -38,7 +40,7 @@ int checkNiederTarif() {
 
 int main() {
 
-  std::cout << currenTimeStr << std::endl;
+  
   if (checkNiederTarif) {
 	std::cout << "Es ist nieder Tarif" << std::endl;
   }
