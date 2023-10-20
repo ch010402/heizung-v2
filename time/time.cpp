55e35e4385484e5de8e874cb54a88d4a512f9515
ch010402 <ZBook@DESKTOP-JDAJDT3>
2023-10-20 20:21:35 +0200

    with ctime

--- time/time.cpp
+++ time/time.cpp
@@ -1,21 +1,15 @@
-#include <chrono>
 #include <iostream>
-#include <strstream>
+#include <chrono>
+#include <ctime>
 
-int hour;
-auto now = std::chrono::system_clock::now();
-std::time_t in_time_t = std::chrono::system_clock::to_time_t(now);
-std::tm buffer;
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-//define something for Windows (32-bit and 64-bit, this part is common)
-localtime_s(&buffer, &in_time_t);
-#elif defined __linux__
-localtime_r(&in_time_t, &buffer);
-#else
-#   error "Unknown compiler"
-#endif
-std::stringstream ss;
-ss << std::put_time(&buffer, "%H");
-ss >> hour;
+int main() {
+  auto currentTime = std::chrono::system_clock::now();
 
-std::cout << hour;
+  std::time_t currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
+
+  std::string currenTimeStr = std::ctime(&currentTime_t);
+
+  std::count << currenTimeStr;
+
+  return 0;
+}
