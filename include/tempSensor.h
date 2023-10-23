369378b85076035f0b56a9a117362f3a97ef5462
ch010402 <christoph.latzer@gmail.com>
2023-10-23 16:51:59 +0200

    added tempaparturSensor.h
    new header for tempSensor

---
+++ include/tempSensor.h
@@ -0,0 +1,29 @@
+/*
+* Header file for the tempSensor class that reads a temperature value of a ds18b20 1-wire temperature sensor
+* Christoph Latzer 2023-10-21
+* heating controls IO's on a raspberryPi that control my house heating
+* this is the second version based on libgpiod as wiringPi is depreciated and no longer in the distro
+* the current version is (libgpiod) v1.6.3 supplied with debian bookworm arm64
+*
+* GNU GPL v3.0
+*/
+
+#ifdef TEMPERATUR_SENSOR_H
+
+#define TEMPERATUR_SENSOR_H
+
+#include <string>
+
+class tempaparturSensor {
+public:
+  tempaparturSensor(std::string sensorName, std::string sensorAddress, double offset);
+  ~tempaparturSensor();
+  
+  double getTemp();
+  std::string getName();
+private:
+  std::string sensorName_;
+  std::string sensorAddresss_;
+  double tempOffset_;
+};
+#endif //!TEMPERATUR_SENSOR_H
